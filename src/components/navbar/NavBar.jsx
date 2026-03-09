80754af8af33837260e9fc7c5a09850c867bbd00
Simon Dubé <simon.dube@c3rios.com>
2026-03-09 11:51:54 -0400

    Display current view mode in the nav bar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -9,6 +9,11 @@ function NavBar() {
       <Link to="/transactions">Transactions</Link>
       <Link to="/charts">Charts</Link>
       <Link to="/import">Import</Link>
+      <p>Current view mode: </p>
+      <p className="mobile">Mobile</p>
+      <p className="tablet">Tablet</p>
+      <p className="laptop">Laptop</p>
+      <p className="desktop">Desktop</p>
     </nav>
   );
 }
