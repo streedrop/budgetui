0588730b9ed96c708efc1e331f9a1c97217cde1f
Simon Dubé <simon.dube@c3rios.com>
2026-04-08 14:37:07 -0400

    Add categories and keywords/rules to the seeder

--- src/pages/devtools/Dev.jsx
+++ src/pages/devtools/Dev.jsx
@@ -1,6 +1,8 @@
 import styles from './styles/Dev.module.css';
 
 import { useTransactions } from '@/hooks/rq/useTransactions.js';
+import { useCategories } from '@/hooks/useCategories.js';
+import { useKeywords } from '@/hooks/useKeywords.js';
 
 import ColorPicker from '@/components/ColorPicker/ColorPicker';
 import IconPicker from '@/components/IconPicker/IconPicker';
@@ -9,15 +11,45 @@ import Button from '@/components/buttons/Button';
 function Dev() {
 
     const { data: transactions = [] } = useTransactions();
+    const { categories } = useCategories();
+    const { keywords: rules } = useKeywords();
 
-    const extractSql = () => {
-        let string = 'INSERT INTO transactions (amount, description, category_id, date) VALUES\n';
-        string = transactions.reduce(((acc, item) => {
-            item.description = item.description.replaceAll("'", "''");
-            acc += `(${Number(item.amount)}, '${item.description.trim()}', ${item.category_id}, '${item.date.split("T")[0]}'),\n`;
+    const generateSeeder = () => {
+        let rules_str = 'INSERT INTO rules (source, match_type, keyword, action, category_id, new_string) VALUES\n';
+        rules_str = rules.reduce(((acc, item) => {
+            item.category_id = item.category_id ? Number(item.category_id) : null;
+            item.new_string = item.new_string === null ? null : `'${item.new_string.replaceAll("'", "''")}'`;
+            item.keyword = `'${item.keyword.replaceAll("'", "''")}'`;
+
+            acc += `('${item.source}', '${item.match_type}', ${item.keyword},  '${item.action}', ${item.category_id}, ${item.new_string}),\n`;
+
             return acc;
-        }), string)
-        navigator.clipboard.writeText(string.slice(0, -2) + ';');
+        }), rules_str);
+
+        let transactions_str = 'INSERT INTO transactions (amount, description, category_id, date) VALUES\n';
+        transactions_str = transactions.reduce(((acc, item) => {
+            item.description = item.description.replaceAll("'", "''");
+
+            acc += `(${Number(item.amount)}, '${item.description.trim()}', ${item.category_id}, '${item.date.split("T")[0]}'),\n`;
+        
+            return acc;
+        }), transactions_str);
+
+        let categories_str = 'INSERT INTO categories (id, name, description, is_income, icon) VALUES\n';
+        categories_str = categories.reduce(((acc, item) => {
+            if(item.name == "Uncategorized")
+                return acc;
+            item.name = item.name.replaceAll("'", "''");
+            item.description = item.description ? `'${item.description.replaceAll("'", "''")}'` : null;
+
+            acc += `(${item.id}, '${item.name.trim()}', ${item.description}, ${Number(item.is_income)}, ${Number(item.icon)}),\n`;
+
+            return acc;
+        }), categories_str);
+
+        const seeder = categories_str.slice(0, -2) + ";\n\n" + rules_str.slice(0, -2) + ";\n\n" + transactions_str.slice(0, -2) + ';\n';
+
+        navigator.clipboard.writeText(seeder);
     }
 
     return (
@@ -27,7 +59,7 @@ function Dev() {
             <IconPicker />
 
             <div>
-                <Button action={extractSql}>Extract transactions as SQL</Button>
+                <Button action={generateSeeder}>Generate SQL Seeder</Button>
             </div>
 
         </>
